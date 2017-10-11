3f84023c235641e727aeed3445ad41bd60c83a5e
amjad-desai <amjad@ubermotion.ae>
2017-10-11 13:23:25 +0400

    Node app setup

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
+//port to be used by Heroku
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
